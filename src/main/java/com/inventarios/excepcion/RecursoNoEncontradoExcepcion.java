f8bf7127728efed7fcfe8046e34dd5bac64ef726
lisandro-kruger <lisandro.kruger@outlook.es>
2023-09-12 10:40:57 -0300

    20230912

---
+++ src/main/java/com/inventarios/excepcion/RecursoNoEncontradoExcepcion.java
@@ -0,0 +1,16 @@
+package com.inventarios.excepcion;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class RecursoNoEncontradoExcepcion extends RuntimeException{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public RecursoNoEncontradoExcepcion(String mensaje){
+        super(mensaje);
+    }
+}
---
+++ src/main/java/com/inventarios/servicio/IProductoServicio.java
@@ -0,0 +1,15 @@
+package com.inventarios.servicio;
+
+import com.inventarios.modelo.Producto;
+import java.util.List;
+
+public interface IProductoServicio {
+    public List<Producto> listarProductos();
+
+    public Producto buscarProductoPorId(Integer idProducto);
+
+    public Producto guardarProducto(Producto producto);
+
+    public void eliminarProductoPorId(Integer idProducto);
+
+}
